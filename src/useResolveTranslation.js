6e95004d695f92158727d7f51fed05b3231ab4a9
Chernukha21 <Chorniy2008@ukr.net>
2022-10-05 22:45:20 +0300

    Added new line to JSON

--- src/useResolveTranslation.js
+++ src/useResolveTranslation.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {useSelector} from "react-redux";
 
-const resolveTranslation = tKey => {
+const useResolveTranslation = tKey => {
     const translation = useSelector(state => state.translation) || {};
     console.log({tKey, translation})
     return translation[tKey]
 };
 
-export default Text;
+export default useResolveTranslation;
